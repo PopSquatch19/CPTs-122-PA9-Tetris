b09c7d061cfc05a00519bc4443e3707c47ed3a36
PopSquatch19 <83146567+PopSquatch19@users.noreply.github.com>
2021-04-29 22:24:41 -0700

    Add files via upload

---
+++ Game.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "SFML/Window.hpp"
+#include "SFML/Graphics.hpp"
+
+class Game
+{
+public:
+
+	static void Start();
+
+	
+
+private:
+	static bool isExiting();
+	static void GameLoop();
+
+
+	static void ShowSplashScreen(); // might exclude not testing now..
+	static void ShowMenu();
+
+	enum GameState {Uninitialized, ShowingSplash, Paused, ShowingMenu, Playing, Exiting}; //might take out ShowingSplash 
+
+	static GameState _gameState;
+	static sf::RenderWindow _mainWindow;
+
+
+
+};
